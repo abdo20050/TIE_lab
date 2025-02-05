c81fc5964dee98403c31e70c08fd6274115c5b83
abdo25 <aabdo20050@gmail.com>
2025-02-05 14:23:47 +0300

    add functions

--- TIE_lab.ino
+++ TIE_lab.ino
@@ -1,105 +1,94 @@
-#define SpeedPin_1 5
-#define AIN1 7
-#define SpeedPin_2 6
-#define BIN1 8
+// motor A is left motor
+#define SpeedPin_A 6
+#define AIN1 8
+#define SpeedPin_B 5
+#define BIN1 7
 #define Enable_motors 3
 
-int PWM_1;
-int PWM_2;
+int PWM_A;
+int PWM_B;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(SpeedPin_1,OUTPUT);
+  pinMode(SpeedPin_A,OUTPUT);
   pinMode(AIN1,OUTPUT);
-  pinMode(SpeedPin_2, OUTPUT);
+  pinMode(SpeedPin_B, OUTPUT);
   pinMode(BIN1,OUTPUT);
   pinMode(Enable_motors,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(Enable_motors, HIGH);//turn on
-  digitalWrite(AIN1, HIGH);//motor A clockwise
-  digitalWrite(BIN1, HIGH);//moto B clockwise
-  PWM_1 = 50;
-  PWM_2 = 50;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  go_forward(100);
   delay(2000);
-  digitalWrite(Enable_motors, LOW);
-  delay(5000);
-  digitalWrite(Enable_motors, HIGH);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(BIN1, LOW);
-  PWM_1 = 50;
-  PWM_2 = 50;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  rotate_right(100);
   delay(2000);
-  digitalWrite(Enable_motors, LOW);
+  rotate_left(100);
+  delay(2000);
+  stop();
   delay(5000);
 }
 
 
-void Stop(){
+void stop(){
   digitalWrite(Enable_motors, LOW);//turn off
 }
 
-void go_forward(int speed = 100){
+void go_forward(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
   digitalWrite(AIN1, HIGH);//motor A clockwise
   digitalWrite(BIN1, HIGH);//moto B clockwise
-  PWM_1 = speed;
-  PWM_2 = speed;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  PWM_A = speed;
+  PWM_B = speed;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
 
-void go_backward(int speed = 100){
+void go_backward(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
   digitalWrite(AIN1, LOW);//motor A clockwise
   digitalWrite(BIN1, LOW);//moto B clockwise
-  PWM_1 = speed;
-  PWM_2 = speed;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  PWM_A = speed;
+  PWM_B = speed;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
 
-void turn_left(int speed = 100){
+void turn_left(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
   digitalWrite(AIN1, HIGH);//motor A clockwise
   digitalWrite(BIN1, HIGH);//moto B clockwise
-  PWM_1 = speed;
-  PWM_2 = speed*0.75;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  PWM_A = speed*0.75;
+  PWM_B = speed;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
 
-void turn_right(int speed = 100){
+void turn_right(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
   digitalWrite(AIN1, HIGH);//motor A clockwise
   digitalWrite(BIN1, HIGH);//moto B clockwise
-  PWM_1 = speed*0.75;
-  PWM_2 = speed;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  PWM_A = speed;
+  PWM_B = speed*0.75;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
 
-void rotate_left(int speed = 100){
+void rotate_left(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
-  digitalWrite(AIN1, LOW);//motor A clockwise
+  digitalWrite(AIN1, LOW);//motor A cclockwise
   digitalWrite(BIN1, HIGH);//moto B clockwise
-  PWM_1 = speed;
-  PWM_2 = speed;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  PWM_A = speed;
+  PWM_B = speed;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
 
-void rotate_right(int speed = 100){
+void rotate_right(int speed){
   digitalWrite(Enable_motors, HIGH);//turn on
   digitalWrite(AIN1, HIGH);//motor A clockwise
-  digitalWrite(BIN1, LOW);//moto B clockwise
-  PWM_1 = speed;
-  PWM_2 = speed;
-  analogWrite(SpeedPin_1, PWM_1);
-  analogWrite(SpeedPin_2, PWM_2);
+  digitalWrite(BIN1, LOW);//moto B cclockwise
+  PWM_A = speed;
+  PWM_B = speed;
+  analogWrite(SpeedPin_A, PWM_A);
+  analogWrite(SpeedPin_B, PWM_B);
 }
